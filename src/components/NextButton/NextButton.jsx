907ab1e2bb7dc9e8f8c4be3632efc552050f0a00
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-01-26 12:51:19 -0500

    update next button

--- src/components/NextButton/NextButton.jsx
+++ src/components/NextButton/NextButton.jsx
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
+import './NextButton.css';
+
 class NextButton extends Component {
 
   handleClick = (counter, score) => {
     const newCounter = counter + 1;
     const newScore = score + 1;
-
     this.props.handler(newCounter, newScore);
-
   }
+
   render() {
     const { counter, score } = this.props;
     return (
-      <button onClick={() => this.handleClick(counter, score)}>hello</button>
-
+      <div id="btnContainer">
+        <button id="nextBtn" value="Submit" onClick={() => this.handleClick(counter, score)}>Next</button>
+      </div>
     )
   }
-
 }
+
 export default NextButton
